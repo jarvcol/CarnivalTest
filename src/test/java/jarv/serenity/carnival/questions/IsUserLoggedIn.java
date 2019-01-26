3560d6a58a7a9fe0be767992154bbe8449f86cbf
Jorge Rojas <Jorge.Rojas@endava.com>
2019-01-26 14:03:47 -0500

    Adding more validation questions

--- src/test/java/jarv/serenity/carnival/questions/IsUserLoggedIn.java
+++ src/test/java/jarv/serenity/carnival/questions/IsUserLoggedIn.java
@@ -1,19 +1,21 @@
 package jarv.serenity.carnival.questions;
 
+import jarv.serenity.carnival.ui.CarnivalHomePageHeader;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
 import net.serenitybdd.screenplay.annotations.Subject;
 import jarv.serenity.carnival.ui.LoginModal;
 
 @Subject("Checks if error message is displayed at login modal")
-public class IsLoginFailed implements Question<Boolean> {
+public class IsUserLoggedIn implements Question<Boolean> {
 
     @Override
     public Boolean answeredBy(Actor actor) {
-        return LoginModal.LOGIN_ERROR_MESSAGE.resolveFor(actor).isCurrentlyVisible();
+        return CarnivalHomePageHeader.GREETING_HEADER_MESSAGE.resolveFor(actor).isCurrentlyVisible() &&
+                !CarnivalHomePageHeader.SIGN_UP_HEADER_LINK.resolveFor(actor).isCurrentlyVisible();
     }
 
-    public static IsLoginModalOpen isShowingError() {
+    public static IsLoginModalOpen isUserLoggedIn() {
         return new IsLoginModalOpen();
     }
 }
--- src/test/java/jarv/serenity/carnival/questions/builder/LoginQuestions.java
+++ src/test/java/jarv/serenity/carnival/questions/builder/LoginQuestions.java
@@ -3,6 +3,7 @@ package jarv.serenity.carnival.questions.builder;
 import net.serenitybdd.screenplay.Question;
 import jarv.serenity.carnival.questions.IsLoginModalOpen;
 import jarv.serenity.carnival.questions.IsLoginFailed;
+import jarv.serenity.carnival.questions.IsUserLoggedIn;
 
 public class LoginQuestions {
     public static Question<Boolean> IsLoginModalOpen() {
@@ -11,6 +12,10 @@ public class LoginQuestions {
 
     public static Question<Boolean> IsLoginFailed() {
         return new IsLoginFailed();
+    }
+
+    public static Question<Boolean> IsUserLoggedIn() {
+        return new IsUserLoggedIn();
     }
 
 }
